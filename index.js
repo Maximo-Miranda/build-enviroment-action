6a12c48cf74f8ab2376757e5cea61b564e9cdb83
Maximo Miranda <maximomirandah@gmail.com>
2022-12-28 14:27:09 -0500

    added dockerfile validation

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const fs = require( 'fs' )
+
+const checkFileExist = async (filePath) => {
+    try {
+        await fs.promises.access(filePath)
+        core.notice(`File ${filePath} exists`)
+        return true
+    } catch (error) {
+        core.setFailed(`File ${filePath} does not exist`)
+        return false
+    }
+}
 
 (
     async () => {
         try {
-            core.notice('Calling Deuna Action');
+            core.notice('Calling Deuna Test Enviroment Action')
+
+            checkFileExist('README.md')
+            checkFileExist('Dockerfile')
             // `who-to-greet` input defined in action metadata file
-            const nameToGreet = core.getInput('who-to-greet');
-            console.log(`Hello ${nameToGreet}!`);
-            const time = (new Date()).toTimeString();
-            core.setOutput("time", time);
-            // Get the JSON webhook payload for the event that triggered the workflow
-            const payload = JSON.stringify(github.context.payload, undefined, 2)
-            console.log(`The event payload: ${payload}`);
+            //const nameToGreet = core.getInput('who-to-greet');
+            //console.log(`Hello ${nameToGreet}!`);
+            //const time = (new Date()).toTimeString();
+            //core.setOutput("time", time);
+            //// Get the JSON webhook payload for the event that triggered the workflow
+            //const payload = JSON.stringify(github.context.payload, undefined, 2)
+            //console.log(`The event payload: ${payload}`);
           } catch (error) {
-            core.setFailed(error.message);
+            core.setFailed(error.message)
           }
     }
 
